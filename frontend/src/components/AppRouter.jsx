2d965747a01346f74fdcca8fc84240b2cc105329
YAP! <gabriele.maddaloni@gmail.com>
2021-09-02 15:57:41 +0200

    Fixed loop crash in AppRouter

--- frontend/src/components/AppRouter.jsx
+++ frontend/src/components/AppRouter.jsx
@@ -1,17 +1,16 @@
 import React from "react";
+import { useEffect } from "react";
 
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { PrivateRoute } from "../helpers/PrivateRoute";
-import HomePage from "./HomePage";
 
+import HomePage from "./HomePage";
 import LoginScreen from "../screens/LoginScreen";
-import Dashboard from "../screens/Dashboard";
-import { useEffect } from "react";
-import ops from "../operations";
-import { useState } from "react";
 import EditorResults from "../screens/EditorResults";
 import Editor from "./Editor";
 import List from "../screens/List";
+
+import ops from "../operations";
 
 function AppRouter(props) {
   useEffect(() => {
@@ -28,17 +27,21 @@ function AppRouter(props) {
 
   return (
     <Switch>
-      <Route path="/login">
-        <LoginScreen />
-      </Route>
-      <PrivateRoute exact path={`/`} render={() => <Redirect to={`/home`} />} />
+      {/* public routes */}
+      <Route path="/login" component={() => <LoginScreen />} />
 
+      {/* <PrivateRoute exact path={"/"} render={() => <Redirect to={"/home"} />} /> */}
+
+      {/* protected routes */}
       <PrivateRoute path="/home" Component={HomePage} />
       <PrivateRoute path="/Answer" Component={EditorResults} />
       <PrivateRoute path="/Write" Component={Editor} />
       <PrivateRoute path="/Diary" Component={List} />
 
-      <PrivateRoute path={`/`} render={() => <Redirect to={`/home`} />} />
+      {/* <PrivateRoute path={"/"} render={() => <Redirect to={"/home"} />} /> */}
+
+      {/* 404 not found */}
+      <Route path={"/"} component={() => <Redirect to={"/home"} />} />
     </Switch>
   );
 }
